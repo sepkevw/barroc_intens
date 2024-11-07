7bf54428fdc75c64812f68666a24f8e772313dc7
Roetjan <ruchan3002@gmail.com>
2024-11-07 12:05:38 +0100

    Setup Database create and DBset

--- Barroc intens/AppDbContext.cs
+++ Barroc intens/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Barroc_intens.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -10,10 +11,31 @@ namespace Barroc_intens
 {
     internal class AppDbContext : DbContext
     {
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CustomerContactPerson> CustomerContactPersons { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Package> Packages { get; set; }
+        public DbSet<PackageProduct> PackageProducts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseMySql(
                 ConfigurationManager.ConnectionStrings["Database"].ConnectionString, ServerVersion.Parse("8.0.13"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .HasOne(c => c.ContactPerson)
+                .WithOne(cp => cp.Customer)
+                .HasForeignKey<CustomerContactPerson>(cp => cp.CustomerId); // Specify the foreign key here
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
