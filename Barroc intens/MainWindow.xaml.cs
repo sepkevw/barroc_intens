7f9a086c4572ee98e406e74be9d22de35593a866
Roetjan <ruchan3002@gmail.com>
2024-11-28 12:19:59 +0100

    removed unneccesary lines

--- Barroc intens/MainWindow.xaml.cs
+++ Barroc intens/MainWindow.xaml.cs
@@ -1,22 +1,5 @@
 using Barroc_intens.Pages;
-using Barroc_intens.Models;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace Barroc_intens
 {
@@ -32,25 +15,7 @@ namespace Barroc_intens
             connection.Database.EnsureDeleted();
             connection.Database.EnsureCreated();
 
-            int RandomId = new Random().Next(1, 25);
-            using(var db = new AppDbContext())  
-            {
-                User LoggedInUser = db.Users.First(u => u.Id == RandomId);
-                //User LoggedInUser = new User
-                //{
-                //    Id = 1,
-                //    Username = "Collin",
-                //    RoleId = 1, // Assuming there are 3 roles
-                //    Created_at = DateTime.Now,
-                //    Role = null
-                //}; Test User
-                this.contentFrame.Navigate(typeof(LoginPage));
-            }
-        }
-
-        private void ReturnButton_Click(object sender, RoutedEventArgs e)
-        {
-
+            contentFrame.Navigate(typeof(LoginPage));
         }
     }
 }
