b88c9e0c4aecc11ff26cf74e9631aeb40dc28926
Roetjan <ruchan3002@gmail.com>
2024-12-02 09:32:48 +0100

    Added SecureHasher for PW and password parameter in User

--- Barroc intens/Models/User.cs
+++ Barroc intens/Models/User.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Barroc_intens.Models
 {
@@ -10,6 +6,7 @@ namespace Barroc_intens.Models
     {
         public int Id { get; set; }
         public string Username { get; set; }
+        public string Password { get; set; }
         public int RoleId { get; set; }
         public DateTime Created_at { get; set; }
         public Role Role { get; set; }
