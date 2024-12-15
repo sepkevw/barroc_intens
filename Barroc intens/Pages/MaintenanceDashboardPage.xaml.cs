5e969ebafd5fab7144bc04c9bc5da08886038dce
Roetjan <ruchan3002@gmail.com>
2024-12-15 11:16:46 +0100

    added filters for the appointments

--- Barroc intens/Pages/MaintenanceDashboardPage.xaml.cs
+++ Barroc intens/Pages/MaintenanceDashboardPage.xaml.cs
@@ -1,31 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Barroc_intens.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
-using Microsoft.EntityFrameworkCore;
-using Barroc_intens.Models;
-using Windows.UI.Popups;
+using System.Linq;
+using System.Threading.Tasks;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace Barroc_intens.Pages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MaintenanceDashboardPage : Page
     {
         public MaintenanceDashboardPage()
@@ -68,6 +55,31 @@ namespace Barroc_intens.Pages
                 {
                     
                 };
+            }
+        }
+
+        private void FilterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var filterText = FilterTextBox.Text.ToLower();
+            using var conn = new AppDbContext();
+
+            if (string.IsNullOrWhiteSpace(filterText))
+            {
+                // If no filter, reset to original data
+                dashboardListView.ItemsSource = conn.Appointments.ToList();
+            }
+            else
+            {
+                // Apply filtering on Name (Description) and Date
+                var filteredAppointments = conn.Appointments.ToList()
+                    .Where(a =>
+                        (a.Description != null && a.Description.ToLower().Contains(filterText)) ||
+                        (a.Date != null && a.Date.ToString("yyyy-MM-dd").Contains(filterText)) ||
+                        (a.Location != null && a.Location.ToLower().Contains(filterText)) || 
+                        (a.Duration != null && a.Duration.ToString().Contains(filterText)) )
+                    .ToList();
+
+                dashboardListView.ItemsSource = filteredAppointments;
             }
         }
     }
